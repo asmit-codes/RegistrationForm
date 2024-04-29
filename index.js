45a5ac797de3688ebd974f62583a757d7b049cd5
asmit-codes <info.asmitkumar@gmail.com>
2024-04-29 20:36:52 +0530

    comit 4

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var mongoose = require("mongoose");
 var userRoutes = require('./routes/userRoutes')
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static('pages'));
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
     extended: true
 }));
 
