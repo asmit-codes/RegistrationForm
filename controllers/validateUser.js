2c2bc4584d8d9a807538a35d83426a489efcb182
asmit-codes <info.asmitkumar@gmail.com>
2024-04-29 19:23:38 +0530

    Update validateUser.js

--- controllers/validateUser.js
+++ controllers/validateUser.js
@@ -1,17 +1,20 @@
 const User = require("../models/User.js");
 exports.validateUser = async (req, res) => {
-  const { fullName, email, phone, password,  } = req.body;
+  const { fullName, email, phone, password, cpwd  } = req.body;
   console.log(fullName, email, phone, password,);
+  // Check for an existing user with the same email
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    res.redirect("error");
+    return res.redirect("error");
   }
+  // Check if passwords match
   if (password !== cpwd) {
-    res.redirect("error");
+    return res.redirect("error");
   }
+  // Check if user was successfully created
   const user = await User.create(req.body);
   if (!user) {
-    res.redirect("error");
+    return res.redirect("error");
   }
-  res.redirect("success");
+  return res.redirect("success");
 };
